2ea18e28e8474786e29feb6cc4331e0af094acd9
Tobias Brunsch <tobias.brunsch@aol.de>
2019-03-05 22:07:53 +0100

    MOD ReflectionUtils: Removed methods only required by Marple

--- zenodot/src/dd/kms/zenodot/common/ReflectionUtils.java
+++ zenodot/src/dd/kms/zenodot/common/ReflectionUtils.java
@@ -109,64 +109,8 @@ public class ReflectionUtils
 	}
 
 	/*
-	 * Classes
-	 */
-	public static Collection<Class<?>> getImplementedClasses(Object object, boolean includeInterfaces) {
-		if (object == null) {
-			return Collections.emptyList();
-		}
-		List<Class<?>> classesToExamine = new ArrayList<>();
-		classesToExamine.add(object.getClass());
-		Set<Class<?>> implementedClasses = new LinkedHashSet<>();
-		while (!classesToExamine.isEmpty()) {
-			Class<?> clazz = classesToExamine.get(0);
-			classesToExamine.remove(0);
-			if (implementedClasses.contains(clazz)) {
-				continue;
-			}
-			implementedClasses.add(clazz);
-			Class<?> superClass = clazz.getSuperclass();
-			if (superClass != null) {
-				classesToExamine.add(superClass);
-			}
-			if (includeInterfaces) {
-				classesToExamine.addAll(Arrays.asList(clazz.getInterfaces()));
-			}
-		}
-		return implementedClasses;
-	}
-
-	/*
 	 * Fields
 	 */
-
-	/**
-	 * Scans the class hierarchy of instance's class to find all fields
-	 * whose values (for this instance) are one of the specified values.
-	 * Returns a map field value -> all fields with this value.
-	 */
-	public static <T> Map<T, List<Field>> findFieldValues(Object instance, Set<T> fieldValues) {
-		Map<T, List<Field>> fieldsByValue = new HashMap<>();
-		if (instance != null) {
-			List<Field> fields = getFields(instance.getClass(), false);
-			for (Field field : fields) {
-				try {
-					field.setAccessible(true);
-					Object fieldValue = field.get(instance);
-					if (fieldValues.contains(fieldValue)) {
-						if (!fieldsByValue.containsKey(fieldValue)) {
-							fieldsByValue.put((T) fieldValue, new ArrayList<>());
-						}
-						fieldsByValue.get(fieldValue).add(field);
-					}
-				} catch (IllegalAccessException e) {
-					/* do nothing */
-				}
-			}
-		}
-		return fieldsByValue;
-	}
-
 	public static List<Field> getFields(Class<?> clazz, boolean filterShadowedFields) {
 		return getFields(clazz, filterShadowedFields, modifiers -> true);
 	}
