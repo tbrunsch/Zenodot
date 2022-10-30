37e037917a4b18814ca625d9b845a6057a94aa1e
Tobias Brunsch <tobias.brunsch@aol.de>
2022-10-30 23:08:23 +0100

    ADD LambdaSample: Sample that demonstrates how lambdas are parsed

---
+++ zenodot-samples/src/dd/kms/zenodot/samples/LambdaSample.java
@@ -0,0 +1,25 @@
+package dd.kms.zenodot.samples;
+
+import dd.kms.zenodot.api.ExpressionParser;
+import dd.kms.zenodot.api.ParseException;
+import dd.kms.zenodot.api.Parsers;
+import dd.kms.zenodot.api.settings.ParserSettings;
+import dd.kms.zenodot.api.settings.ParserSettingsBuilder;
+
+import java.util.Collections;
+
+/**
+ * This sample demonstrates how to evaluate lambdas
+ */
+public class LambdaSample
+{
+	public static void main(String[] args) throws ParseException {
+		ParserSettings settings = ParserSettingsBuilder.create()
+			.importPackages(Collections.singletonList("java.util"))
+			.build();
+		// s must be cast to String because Zenodot does not infer generic types
+		String expression = "Arrays.asList(\"1\", \"2\", \"3\").stream().mapToInt(s -> Integer.parseInt((String) s)).sum()";
+		ExpressionParser parser = Parsers.createExpressionParser(settings);
+		System.out.println("Result: " + parser.evaluate(expression, null));
+	}
+}
