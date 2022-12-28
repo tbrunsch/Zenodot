765e77eaac01cfe40921df52c43067ece76f93df
Tobias Brunsch <tobias.brunsch@aol.de>
2022-12-28 18:15:19 +0100

    MOD ExpressionParserBuilder: Documented methods

--- zenodot/src/dd/kms/zenodot/api/ExpressionParserBuilder.java
+++ zenodot/src/dd/kms/zenodot/api/ExpressionParserBuilder.java
@@ -2,8 +2,23 @@ package dd.kms.zenodot.api;
 
 public interface ExpressionParserBuilder
 {
+	/**
+	 * Registers the variables that can be accessed and possibly overwritten by the expression to parse.
+	 */
 	ExpressionParserBuilder variables(Variables variables);
+
 	ExpressionParser createExpressionParser();
+
+	/**
+	 * Creates a parser for a lambda that represents the functional interface {@code functionalInterface}.
+	 */
 	ExpressionParser createLambdaParser(Class<?> functionalInterface);
+
+	/**
+	 * Creates a parser for a lambda that represents the functional interface {@code functionalInterface}.
+	 * With {@code parameterTypes} you can specify the exact types of the parameters of the method to
+	 * implement. This is necessary if the functional interface is generic or extends a generic interface
+	 * in order to avoid casts in the lambda body.
+	 */
 	ExpressionParser createLambdaParser(Class<?> functionalInterface, Class<?>... parameterTypes);
 }
