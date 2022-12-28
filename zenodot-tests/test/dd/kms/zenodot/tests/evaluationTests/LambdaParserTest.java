db133611841ba0bfa15cdf0bd931f57046f662bb
Tobias Brunsch <tobias.brunsch@aol.de>
2022-12-28 13:52:10 +0100

    MOD LambdaParserTest: Added further tests

--- zenodot-tests/test/dd/kms/zenodot/tests/evaluationTests/LambdaParserTest.java
+++ zenodot-tests/test/dd/kms/zenodot/tests/evaluationTests/LambdaParserTest.java
@@ -1,20 +1,44 @@
 package dd.kms.zenodot.tests.evaluationTests;
 
+import com.google.common.collect.ImmutableList;
 import dd.kms.zenodot.api.CompiledExpression;
 import dd.kms.zenodot.api.ExpressionParser;
-import dd.kms.zenodot.api.ParseException;
 import dd.kms.zenodot.api.Parsers;
 import dd.kms.zenodot.api.settings.ParserSettings;
 import dd.kms.zenodot.api.settings.ParserSettingsBuilder;
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.util.Arrays;
-import java.util.Comparator;
+import java.util.*;
+import java.util.function.Consumer;
 import java.util.function.Function;
+import java.util.function.Supplier;
 
 public class LambdaParserTest
 {
+	@Test
+	public void testSupplier() throws Exception {
+		final long seed = 1234567890L;
+		Random random1 = new Random(seed);
+		Random random2 = new Random(seed);
+
+		Supplier<Object> supplier = parseLambda(Supplier.class, "() -> this.nextDouble()", random1);
+		for (int i = 0; i < 100; i++) {
+			Assert.assertEquals(random2.nextDouble(), supplier.get());
+		}
+	}
+
+	@Test
+	public void testConsumer() throws Exception {
+		List<String> tokens = new ArrayList<>();
+		Consumer<Object> consumer = parseLambda(Consumer.class, "s -> this.add(s)", tokens);
+		List<String> expectedTokens = ImmutableList.of("This", " ", "is", " ", "a", " ", "test", ".");
+		for (String token : expectedTokens) {
+			consumer.accept(token);
+		}
+		Assert.assertEquals(expectedTokens, tokens);
+	}
+
 	@Test
 	public void testFunction() throws Exception {
 		Function<Object, Object> function = parseLambda(Function.class, "t -> ((String) t).length()");
@@ -36,10 +60,14 @@ public class LambdaParserTest
 	}
 
 	private static <T> T parseLambda(Class<T> functionalInterface, String lambdaExpression) throws Exception {
+		return parseLambda(functionalInterface, lambdaExpression, null);
+	}
+
+	private static <T> T parseLambda(Class<T> functionalInterface, String lambdaExpression, Object thisValue) throws Exception {
 		ParserSettings parserSettings = ParserSettingsBuilder.create().build();
 		ExpressionParser lambdaParser = Parsers.createExpressionParserBuilder(parserSettings).createLambdaParser(functionalInterface);
-		CompiledExpression compiledExpression = lambdaParser.compile(lambdaExpression, null);
-		Object result = compiledExpression.evaluate(null);
+		CompiledExpression compiledExpression = lambdaParser.compile(lambdaExpression, thisValue);
+		Object result = compiledExpression.evaluate(thisValue);
 		Assert.assertTrue("parsed result must be of type '" + functionalInterface.getName() + "'", functionalInterface.isInstance(result));
 		return functionalInterface.cast(result);
 	}
