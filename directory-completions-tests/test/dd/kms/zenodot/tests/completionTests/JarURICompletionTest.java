147777286ae5fe4e0a3c1ad5e3794e0f141004b4
Tobias Brunsch <tobias.brunsch@aol.de>
2023-12-21 17:43:21 +0100

    MOD JarURICompletionTest: Test archive files with spaces in the name

--- directory-completions-tests/test/dd/kms/zenodot/tests/completionTests/JarURICompletionTest.java
+++ directory-completions-tests/test/dd/kms/zenodot/tests/completionTests/JarURICompletionTest.java
@@ -34,11 +34,11 @@ public class JarURICompletionTest extends CompletionTest
 	@BeforeClass
 	public static void setupFileSystems() throws IOException {
 		NONE_DEFAULT_FILE_SYSTEM = DirectoryCompletionTestUtils.createFileSystem("jaruri");
-		Path archiveFile = NONE_DEFAULT_FILE_SYSTEM.getPath("/").resolve("archive.jar");
+		Path archiveFile = NONE_DEFAULT_FILE_SYSTEM.getPath("/").resolve("my archive.jar");
 		try (OutputStream os = Files.newOutputStream(archiveFile, StandardOpenOption.CREATE);
 			ZipOutputStream zos = new ZipOutputStream(os)) {
 			zos.close();
-			URI archiveRootURI = URI.create("jar:jimfs://jaruri/archive.jar!/");
+			URI archiveRootURI = URI.create("jar:jimfs://jaruri/my%20archive.jar!/");
 			try (FileSystem jarFileSystem = FileSystems.newFileSystem(archiveRootURI, ImmutableMap.of("create", "true"))) {
 				FileSystemUtils.setupFileSystem(jarFileSystem, DirectoryCompletionTestUtils.ROOT);
 			}
@@ -53,7 +53,7 @@ public class JarURICompletionTest extends CompletionTest
 	@Parameterized.Parameters(name = "{0}")
 	public static Collection<Object> getTestData() {
 		// see DirectoryCompletionTestUtils.ROOT for the directory hierarchy for which Zenodot provides completions
-		URI uri = URI.create("jar:jimfs://jaruri/archive.jar!/zenodot");
+		URI uri = URI.create("jar:jimfs://jaruri/my%20archive.jar!/zenodot");
 		System.out.println(uri.getSchemeSpecificPart());
 		return new CompletionTestBuilder()
 			.configurator(test -> {
@@ -61,32 +61,32 @@ public class JarURICompletionTest extends CompletionTest
 				test.importClasses("java.net.URI");
 				registerURICompletions(test.getSettingsBuilder());
 			})
-			.addTest("new URI(\"jar:jimfs://jaruri/archive.jar!/", "zenodot")
-			.addTest("new URI(\"jar:jimfs://jaruri/archive.jar!/zen", "zenodot")
-			.addTest("new URI(\"jar:jimfs://jaruri/archive.jar!/zenodot/fr", "framework")
-			.addTest("new URI(\"jar:jimfs://jaruri/archive.jar!/zenodot/dir", "directory%20with%20spaces")
-			.addTest("new URI(\"jar:jimfs://jaruri/archive.jar!/zenodot/directory%20with%20spaces/file", "file%20with%20spaces")
+			.addTest("new URI(\"jar:jimfs://jaruri/my%20archive.jar!/", "zenodot")
+			.addTest("new URI(\"jar:jimfs://jaruri/my%20archive.jar!/zen", "zenodot")
+			.addTest("new URI(\"jar:jimfs://jaruri/my%20archive.jar!/zenodot/fr", "framework")
+			.addTest("new URI(\"jar:jimfs://jaruri/my%20archive.jar!/zenodot/dir", "directory%20with%20spaces")
+			.addTest("new URI(\"jar:jimfs://jaruri/my%20archive.jar!/zenodot/directory%20with%20spaces/file", "file%20with%20spaces")
 
-			.addTest("new URI(\"jar\", \"jimfs://jaruri/archive.jar!/zenodot/a", "api")
-			.addTest("new URI(\"jar\", \"jimfs://jaruri/archive.jar!/zenodot/api/Ex", "ExpressionParser")
-			.addTest("new URI(\"jar\", \"jimfs://jaruri/archive.jar!/zenodot/directory with spaces/f", "file with spaces")
+			.addTest("new URI(\"jar\", \"jimfs://jaruri/my archive.jar!/zenodot/a", "api")
+			.addTest("new URI(\"jar\", \"jimfs://jaruri/my archive.jar!/zenodot/api/Ex", "ExpressionParser")
+			.addTest("new URI(\"jar\", \"jimfs://jaruri/my archive.jar!/zenodot/directory with spaces/f", "file with spaces")
 
-			.addTest("URI.create(\"jar:jimfs://jaruri/archive.jar!/", "zenodot")
-			.addTest("URI.create(\"jar:jimfs://jaruri/archive.jar!/zen", "zenodot")
-			.addTest("URI.create(\"jar:jimfs://jaruri/archive.jar!/zenodot/fr", "framework")
-			.addTest("URI.create(\"jar:jimfs://jaruri/archive.jar!/zenodot/direct", "directory%20with%20spaces")
-			.addTest("URI.create(\"jar:jimfs://jaruri/archive.jar!/zenodot/directory%20with%20spaces/subdirectory_", "subdirectory_without_spaces")
+			.addTest("URI.create(\"jar:jimfs://jaruri/my%20archive.jar!/", "zenodot")
+			.addTest("URI.create(\"jar:jimfs://jaruri/my%20archive.jar!/zen", "zenodot")
+			.addTest("URI.create(\"jar:jimfs://jaruri/my%20archive.jar!/zenodot/fr", "framework")
+			.addTest("URI.create(\"jar:jimfs://jaruri/my%20archive.jar!/zenodot/direct", "directory%20with%20spaces")
+			.addTest("URI.create(\"jar:jimfs://jaruri/my%20archive.jar!/zenodot/directory%20with%20spaces/subdirectory_", "subdirectory_without_spaces")
 
-			.addInsertionTest("new URI(\"jar:jimfs://jaruri/archive.jar!/zen^/fr",					'^', "new URI(\"jar:jimfs://jaruri/archive.jar!/zenodot/fr")
-			.addInsertionTest("new URI(\"jar:jimfs://jaruri/archive.jar!/zenodot/dir^ectory/file",	'^', "new URI(\"jar:jimfs://jaruri/archive.jar!/zenodot/directory%20with%20spaces/file")
+			.addInsertionTest("new URI(\"jar:jimfs://jaruri/my%20archive.jar!/zen^/fr",					'^', "new URI(\"jar:jimfs://jaruri/my%20archive.jar!/zenodot/fr")
+			.addInsertionTest("new URI(\"jar:jimfs://jaruri/my%20archive.jar!/zenodot/dir^ectory/file",	'^', "new URI(\"jar:jimfs://jaruri/my%20archive.jar!/zenodot/directory%20with%20spaces/file")
 
-			.addInsertionTest("new URI(\"jar\", \"jimfs://jaruri/archive.jar!/z^/a", 					'^', "new URI(\"jar\", \"jimfs://jaruri/archive.jar!/zenodot/a")
-			.addInsertionTest("new URI(\"jar\", \"jimfs://jaruri/archive.jar!/zenod^/api/Ex",			'^', "new URI(\"jar\", \"jimfs://jaruri/archive.jar!/zenodot/api/Ex")
-			.addInsertionTest("new URI(\"jar\", \"jimfs://jaruri/archive.jar!/zenodot/a^/Ex",			'^', "new URI(\"jar\", \"jimfs://jaruri/archive.jar!/zenodot/api/Ex")
-			.addInsertionTest("new URI(\"jar\", \"jimfs://jaruri/archive.jar!/zenodot/dir^ectory/sub",	'^', "new URI(\"jar\", \"jimfs://jaruri/archive.jar!/zenodot/directory with spaces/sub")
+			.addInsertionTest("new URI(\"jar\", \"jimfs://jaruri/my archive.jar!/z^/a", 					'^', "new URI(\"jar\", \"jimfs://jaruri/my archive.jar!/zenodot/a")
+			.addInsertionTest("new URI(\"jar\", \"jimfs://jaruri/my archive.jar!/zenod^/api/Ex",			'^', "new URI(\"jar\", \"jimfs://jaruri/my archive.jar!/zenodot/api/Ex")
+			.addInsertionTest("new URI(\"jar\", \"jimfs://jaruri/my archive.jar!/zenodot/a^/Ex",			'^', "new URI(\"jar\", \"jimfs://jaruri/my archive.jar!/zenodot/api/Ex")
+			.addInsertionTest("new URI(\"jar\", \"jimfs://jaruri/my archive.jar!/zenodot/dir^ectory/sub",	'^', "new URI(\"jar\", \"jimfs://jaruri/my archive.jar!/zenodot/directory with spaces/sub")
 
-			.addInsertionTest("URI.create(\"jar:jimfs://jaruri/archive.jar!/zen^/fr",					'^', "URI.create(\"jar:jimfs://jaruri/archive.jar!/zenodot/fr")
-			.addInsertionTest("URI.create(\"jar:jimfs://jaruri/archive.jar!/zenodot/dir^ectory/file",	'^', "URI.create(\"jar:jimfs://jaruri/archive.jar!/zenodot/directory%20with%20spaces/file")
+			.addInsertionTest("URI.create(\"jar:jimfs://jaruri/my%20archive.jar!/zen^/fr",					'^', "URI.create(\"jar:jimfs://jaruri/my%20archive.jar!/zenodot/fr")
+			.addInsertionTest("URI.create(\"jar:jimfs://jaruri/my%20archive.jar!/zenodot/dir^ectory/file",	'^', "URI.create(\"jar:jimfs://jaruri/my%20archive.jar!/zenodot/directory%20with%20spaces/file")
 
 			.build();
 	}
