0c89721b45655df34c3cdc6876e0f2fdcac5fcf8
Tobias Brunsch <tobias.brunsch@aol.de>
2025-06-12 19:23:37 +0200

    Added MethodTest

---
+++ zenodot-x-tests/test/dd/kms/zenodotx/tests/evaluation/MethodTest.java
@@ -0,0 +1,36 @@
+package dd.kms.zenodotx.tests.evaluation;
+
+import dd.kms.zenodotx.tests.evaluation.framework.EvaluationTest;
+import dd.kms.zenodotx.tests.evaluation.framework.EvaluationTestBuilder;
+import dd.kms.zenodotx.tests.evaluation.framework.TestData;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+import java.util.Collection;
+
+@RunWith(Parameterized.class)
+public class MethodTest extends EvaluationTest
+{
+	public MethodTest(TestData testData) {
+		super(testData);
+	}
+
+	@Parameters(name = "{0}")
+	public static Collection<Object> getTestData() {
+		Object testInstance = new TestClass();
+		return new EvaluationTestBuilder()
+			.testInstance(testInstance)
+			.addTest("getInt()",	3)
+			.addTest("getDouble()",	2.7)
+			.addTest("getString()",	"xyz")
+			.build();
+	}
+
+	private static class TestClass
+	{
+		int getInt() { return 3; }
+		double getDouble() { return 2.7; }
+		String getString() { return "xyz"; }
+	}
+}
