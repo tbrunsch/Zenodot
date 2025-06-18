c1aded3223dddbc494e8ee6730ee371e664b852d
Tobias Brunsch <tobias.brunsch@aol.de>
2025-06-18 20:25:49 +0200

    OrRuleImpl: Evaluate all alternatives and then decide depending on how far each alternative could be parsed syntactically which was the correct alternative

--- zenodot-x/src/dd/kms/zenodotx/Parser.java
+++ zenodot-x/src/dd/kms/zenodotx/Parser.java
@@ -39,15 +39,8 @@ public class Parser<S>
 	}
 
 	private <I, O> O parseSimpleRule(SimpleRule<I, O, S> rule, I input, S settings) throws SyntaxException, SemanticException, EvaluationException, EventResultException {
-		SyntaxRule syntaxRule = rule.getSyntaxRule();
-		// TODO: Whitespace handling
-		// TODO: Not clear whether every case can be handled by a regex. What if code completion is
-		//       requested, but the regex does not match the current string? What to do the code completion
-		//       on then?
 		int positionBeforeRegex = characterStream.getPosition();
-		Pattern regex = syntaxRule.getRegex();
-		// TODO: What should the message be?
-		String parsedString = characterStream.readRegex(regex).orElseThrow(SyntaxException::new);
+		String parsedString = parseSimpleRuleSyntactically(rule);
 		int positionAfterRegex = characterStream.getPosition();
 
 		SemanticRule<I, O, S> semanticRule = rule.getSemanticRule();
@@ -61,6 +54,28 @@ public class Parser<S>
 		}
 
 		return semanticRule.evaluate(input, parsedString, settings);
+	}
+
+	public void parseSyntactically(Rule<?, ?, S> rule) throws SyntaxException {
+		if (rule instanceof CompoundRule) {
+			((CompoundRule<?, ?, S>) rule).parseSyntactically(this);
+		} else if (rule instanceof SimpleRule) {
+			parseSimpleRuleSyntactically((SimpleRule<?, ?, S>) rule);
+		} else {
+			throw new IllegalStateException("Cannot parse rule of type " + rule.getClass().getName()
+				+ ". Only " + CompoundRule.class.getName() + " and " + SimpleRule.class.getName() + " are supported.");
+		}
+	}
+
+	private String parseSimpleRuleSyntactically(SimpleRule<?, ?, S> rule) throws SyntaxException {
+		SyntaxRule syntaxRule = rule.getSyntaxRule();
+		// TODO: Whitespace handling
+		// TODO: Not clear whether every case can be handled by a regex. What if code completion is
+		//       requested, but the regex does not match the current string? What to do the code completion
+		//       on then?
+		Pattern regex = syntaxRule.getRegex();
+		// TODO: What should the message be?
+		return characterStream.readRegex(regex).orElseThrow(SyntaxException::new);
 	}
 
 	public void storeState() {
--- zenodot-x/src/dd/kms/zenodotx/rule/compound/OrRuleImpl.java
+++ zenodot-x/src/dd/kms/zenodotx/rule/compound/OrRuleImpl.java
@@ -10,6 +10,7 @@ import dd.kms.zenodotx.rule.Rule;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.IntSupplier;
 
 public class OrRuleImpl<I, O, S> extends AbstractRule<I, O, S> implements OrRule<I, O, S>
 {
@@ -23,41 +24,41 @@ public class OrRuleImpl<I, O, S> extends AbstractRule<I, O, S> implements OrRule
 
 	@Override
 	public O parse(I input, S settings, Parser<S> parser) throws SyntaxException, EvaluationException, SemanticException, Parser.EventResultException {
-		List<SyntaxException> syntaxExceptions = new ArrayList<>();
-		List<SemanticException> semanticExceptions = new ArrayList<>();
-		List<Parser.EventResultException> eventResultExceptions = new ArrayList<>();
+		if (alternatives.isEmpty()) {
+			// TODO: Add error message: No alternatives defined for this or rule
+			throw new SyntaxException();
+		}
+		ResultAggregator<O> resultAggregator = new ResultAggregator<>();
 		for (Rule<I, O, S> alternative : alternatives) {
 			parser.storeState();
 			try {
-				return parser.parse(alternative, input, settings);
+				O result = parser.parse(alternative, input, settings);
+				resultAggregator.addResult(result, parser.getParsePosition());
 			} catch (SyntaxException e) {
-				// continue with next alternative
-				syntaxExceptions.add(e);
+				resultAggregator.addSyntaxException(e, parser.getParsePosition());
 			} catch (SemanticException e) {
-				// continue with next alternative
-				semanticExceptions.add(e);
+				resultAggregator.addSemanticException(e, () -> {
+					parser.restoreState();
+					parser.storeState();
+					try {
+						parser.parseSyntactically(alternative);
+					} catch (SyntaxException ignored) {
+						/* does not matter */
+					}
+					return parser.getParsePosition();
+				});
 			} catch (Parser.EventResultException e) {
-				// continue with next alternative
-				eventResultExceptions.add(e);
+				resultAggregator.addEventResultException(e);
 			}
 			/*
-			 * An EvaluationException is directly propagated to the caller because it means that we have found
-			 * the correct alternative.
+			 * There is no catch for EvaluationExceptions because such Exceptions mean that we already found the
+			 * correct alternative, but it could not be evaluated.
 			 */
 			parser.restoreState();
 		}
-		if (!eventResultExceptions.isEmpty()) {
-			// TODO: Merge event result exceptions
-			throw eventResultExceptions.get(0);
-		} else if (!semanticExceptions.isEmpty()) {
-			// TODO: Merge them
-			throw semanticExceptions.get(0);
-		} else if (!syntaxExceptions.isEmpty()) {
-			// TODO: Merge them
-			throw syntaxExceptions.get(0);
-		} else {
-			throw new IllegalStateException("Or rule does not have any alternatives");
-		}
+		int aggregatedParsePosition = resultAggregator.getAggregatedParsePosition();
+		parser.setParsePosition(aggregatedParsePosition);
+		return resultAggregator.aggregate();
 	}
 
 	@Override
@@ -107,5 +108,110 @@ public class OrRuleImpl<I, O, S> extends AbstractRule<I, O, S> implements OrRule
 	@Override
 	protected String getGenericName() {
 		return "rule 1 or rule 2 or ...";
+	}
+
+	/**
+	 * Aggregates the outcomes of parsing the different alternatives and eventually decides which alternative was
+	 * (most likely) the correct one. The decision logic is currently as follows:
+	 * <ul>
+	 *     <li>
+	 *         If there are {@link Parser.EventResultException}s (in most cases these will be code completions), then
+	 *         these have the highest priority.
+	 *     </li>
+	 *     <li>
+	 *         Otherwise, the outcome(s) with the highest parse position win(s). It is not clear that this heuristic
+	 *         always yields the correct result, but it will work very often, and we hope that rules can be formulated
+	 *         such that this heuristic works correctly.<br>
+	 *         <br>
+	 *         Note that an alternative that returns a result is not necessarily the correct one. The empty rule, which
+	 *         is used, e.g., as alternative in method and constructor parameter lists, always succeeds, but this is not
+	 *         always the correct rule to use.<br>
+	 *         <br>
+	 *         When multiple types of outcomes have the same parse position, ties are broken as follows:
+	 *         <ol>
+	 *             <li>Results have the highest priority.</li>
+	 *             <li>{@link SemanticException}s have medium priority.</li>
+	 *             <li>{@link SyntaxException}s have the lowest priority.</li>
+	 *         </ol>
+	 *     </li>
+	 * </ul>
+	 */
+	private static class ResultAggregator<O>
+	{
+		private final List<Parser.EventResultException>		eventResultExceptions	= new ArrayList<>();
+		private final List<O>								results					= new ArrayList<>();
+		private final List<SemanticException>				semanticExceptions		= new ArrayList<>();
+		private final List<SyntaxException>					syntaxExceptions		= new ArrayList<>();
+
+		private int	maxParsePosition	= -1;
+
+		public void addEventResultException(Parser.EventResultException eventResultException) {
+			eventResultExceptions.add(eventResultException);
+			results.clear();
+			semanticExceptions.clear();
+			syntaxExceptions.clear();
+		}
+
+		public void addResult(O result, int parsePosition) {
+			if (!eventResultExceptions.isEmpty()) {
+				// eventResultExceptions have the highest priority
+				return;
+			}
+			if (parsePosition >= maxParsePosition) {
+				maxParsePosition = parsePosition;
+				results.add(result);
+				semanticExceptions.clear();
+				syntaxExceptions.clear();
+			}
+		}
+
+		public void addSemanticException(SemanticException semanticException, IntSupplier parsePositionSupplier) {
+			if (!eventResultExceptions.isEmpty()) {
+				// eventResultExceptions have the highest priority
+				return;
+			}
+			int parsePosition = parsePositionSupplier.getAsInt();
+			if (parsePosition > maxParsePosition || parsePosition == maxParsePosition && results.isEmpty()) {
+				maxParsePosition = parsePosition;
+				semanticExceptions.add(semanticException);
+				syntaxExceptions.clear();
+			}
+		}
+
+		public void addSyntaxException(SyntaxException syntaxException, int parsePosition) {
+			if (!eventResultExceptions.isEmpty()) {
+				// eventResultExceptions have the highest priority
+				return;
+			}
+			if (parsePosition > maxParsePosition || parsePosition == maxParsePosition && results.isEmpty() && semanticExceptions.isEmpty()) {
+				maxParsePosition = parsePosition;
+				syntaxExceptions.add(syntaxException);
+			}
+		}
+
+		public int getAggregatedParsePosition() {
+			return maxParsePosition;
+		}
+
+		public O aggregate() throws Parser.EventResultException, SemanticException, SyntaxException {
+			if (!eventResultExceptions.isEmpty()) {
+				// TODO: Merge eventResultExceptions
+				throw eventResultExceptions.get(0);
+			} else if (!results.isEmpty()) {
+				if (results.size() == 1) {
+					return results.get(0);
+				}
+				// TODO: More details required
+				throw new SemanticException("Ambiguous expression");
+			} else if (!semanticExceptions.isEmpty()) {
+				// TODO: Merge semantic exceptions
+				throw semanticExceptions.get(0);
+			} else if (!syntaxExceptions.isEmpty()) {
+				// TODO: Merge syntax exceptions
+				throw syntaxExceptions.get(0);
+			} else {
+				throw new IllegalStateException("The or rule seems to have no alternatives, but this case should have been handled before.");
+			}
+		}
 	}
 }
