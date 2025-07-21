5f4d5737d2db078775ec0aea62840a363fc24710
Tobias Brunsch <tobias.brunsch@aol.de>
2025-07-21 21:56:31 +0200

    Pair: Added equals() and hashCode()

--- zenodot-x/src/dd/kms/zenodotx/common/Pair.java
+++ zenodot-x/src/dd/kms/zenodotx/common/Pair.java
@@ -1,5 +1,7 @@
 package dd.kms.zenodotx.common;
 
+import java.util.Objects;
+
 public class Pair<S, T>
 {
 	private final S	first;
@@ -16,5 +18,17 @@ public class Pair<S, T>
 
 	public T getSecond() {
 		return second;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (o == null || getClass() != o.getClass()) return false;
+		Pair<?, ?> pair = (Pair<?, ?>) o;
+		return Objects.equals(first, pair.first) && Objects.equals(second, pair.second);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(first, second);
 	}
 }
