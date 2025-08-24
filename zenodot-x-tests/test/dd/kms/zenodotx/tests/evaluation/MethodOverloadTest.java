160df14ad70301d7d6a856b66f601d112c0a8a34
Tobias Brunsch <tobias.brunsch@aol.de>
2025-08-24 19:09:53 +0200

    MethodOverloadTest: Added further test cases

--- zenodot-x-tests/test/dd/kms/zenodotx/tests/evaluation/MethodOverloadTest.java
+++ zenodot-x-tests/test/dd/kms/zenodotx/tests/evaluation/MethodOverloadTest.java
@@ -1,5 +1,6 @@
 package dd.kms.zenodotx.tests.evaluation;
 
+import dd.kms.zenodot.api.settings.EvaluationMode;
 import dd.kms.zenodotx.exception.SemanticException;
 import dd.kms.zenodotx.tests.evaluation.framework.EvaluationTest;
 import dd.kms.zenodotx.tests.evaluation.framework.EvaluationTestBuilder;
@@ -19,7 +20,27 @@ public class MethodOverloadTest extends EvaluationTest
 
 	@Parameters(name = "{0}")
 	public static Collection<Object> getTestData() {
-		EvaluationTestBuilder testBuilder = new EvaluationTestBuilder().testInstanceProvider(TestClass::new);
+		TestClass testInstance = new TestClass();
+		EvaluationTestBuilder testBuilder = new EvaluationTestBuilder().testInstanceProvider(() -> testInstance);
+
+		testBuilder
+			.configurator(null)
+			.addTest("get(c)",	testInstance.get(testInstance.c))
+			.addTest("get(b)",	testInstance.get(testInstance.b))
+			.addTest("get(i)",	testInstance.get(testInstance.i))
+			.addTest("get(l)",	testInstance.get(testInstance.l))
+			.addTest("get(o1)",	testInstance.get(testInstance.o1))
+			.addTest("get(o2)",	testInstance.get(testInstance.o2));
+
+		testBuilder
+			.configurator(test -> test.evaluationMode(EvaluationMode.MIXED))
+			.addTest("get(o1)", testInstance.get(testInstance.o1))
+			.addTest("get(o2)", testInstance.get((Float) testInstance.o2));
+
+		testBuilder
+			.configurator(null)
+			// TODO: Is this really an error or is this the same as get((int) s)?
+			.addTestWithError("get(s)", SemanticException.class);
 
 		testBuilder
 			.addTest("getTestClass(myInt).i",		3)
@@ -44,8 +65,23 @@ public class MethodOverloadTest extends EvaluationTest
 
 	private static class TestClass
 	{
-		private final int myInt = 3;
-		private final String myString = "xyz";
+		private final char		c	= 'A';
+		private final byte		b	= 123;
+		private final short		s	= (short) 1234;
+		private final int		i	= 123456789;
+		private final long		l	= 5000000000L;
+		private final Object	o1	= new Double(1.23);
+		private final Object	o2	= new Float(2.34f);
+
+		private final int		myInt		= 3;
+		private final String	myString	= "xyz";
+
+		char get(char c) { return c; }
+		byte get(byte b) { return b; }
+		int get(int l) { return i; }
+		double get(double d) { return d; }
+		Object get(Object o) { return o; }
+		Object get(Float f) { return f + 1.0f; }
 
 		MemberClass1 getTestClass(int i) { return new MemberClass1(); }
 		MemberClass2 getTestClass(String s) { return new MemberClass2(); }
