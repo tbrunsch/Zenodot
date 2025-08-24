18195e59f2670161af2cf40da192e3319e744d18
Tobias Brunsch <tobias.brunsch@aol.de>
2025-08-24 21:20:48 +0200

    MethodOverloadTest: Added details about how method overloading should work in specific cases

--- zenodot-x-tests/test/dd/kms/zenodotx/tests/evaluation/MethodOverloadTest.java
+++ zenodot-x-tests/test/dd/kms/zenodotx/tests/evaluation/MethodOverloadTest.java
@@ -39,7 +39,16 @@ public class MethodOverloadTest extends EvaluationTest
 
 		testBuilder
 			.configurator(null)
-			// TODO: Is this really an error or is this the same as get((int) s)?
+			/*
+			 * TODO: Our current understanding of the Java Specification was that different overloads that both can be
+			 *       called by widening the argument (here short -> int vs. short -> double) are considered equally good.
+			 *       However, it seems that the overload with the narrowest widening is preferred. The types according to
+			 *       preference are short, int, long, float, double. In this example, get(int) should be called.
+			 *
+			 * Potential sources:
+			 *   - JLS §15.12.2.5 – Choosing the Most Specific Method (https://docs.oracle.com/javase/specs/jls/se8/html/jls-15.html#jls-15.12.2.5)
+             *   - JLS §5.1.2 – Widening Primitive Conversion
+			 */
 			.addTestWithError("get(s)", SemanticException.class);
 
 		testBuilder
