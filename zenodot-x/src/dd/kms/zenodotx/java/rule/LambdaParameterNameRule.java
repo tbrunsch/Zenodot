bcb17bddf43362994adf51f4f3af0d4447364df3
Tobias Brunsch <tobias.brunsch@aol.de>
2026-04-28 21:01:29 +0200

    Added rule for parsing lambda parameters

--- zenodot-x/src/dd/kms/zenodotx/java/rule/LambdaParameterNameRule.java
+++ zenodot-x/src/dd/kms/zenodotx/java/rule/LambdaParameterNameRule.java
@@ -1,5 +1,38 @@
 package dd.kms.zenodotx.java.rule;
 
-public class LambdaParameterNameRule extends UnimplementedRule<LambdaParameterInfo, LambdaParameterInfo>
+import dd.kms.zenodotx.exception.EvaluationException;
+import dd.kms.zenodotx.exception.SemanticException;
+import dd.kms.zenodotx.java.JavaSettings;
+import dd.kms.zenodotx.rule.AbstractRule;
+import dd.kms.zenodotx.rule.simple.SemanticRule;
+import dd.kms.zenodotx.rule.simple.SimpleRule;
+import dd.kms.zenodotx.rule.simple.SyntaxRule;
+
+public class LambdaParameterNameRule extends AbstractRule<LambdaParameterInfo, LambdaParameterInfo, JavaSettings> implements SimpleRule<LambdaParameterInfo, LambdaParameterInfo, JavaSettings>
 {
+	@Override
+	public SyntaxRule getSyntaxRule() {
+		return IdentifierSyntaxRule.RULE;
+	}
+
+	@Override
+	public SemanticRule<LambdaParameterInfo, LambdaParameterInfo, JavaSettings> getSemanticRule() {
+		return new AbstractSemanticJavaRule<LambdaParameterInfo, LambdaParameterInfo>() {
+			@Override
+			protected void doSuggestCodeCompletions(LambdaParameterInfo parameterInfo, String parsedString, JavaSettings settings) {
+				// TODO
+			}
+
+			@Override
+			protected void doSuggestMethodParameters(LambdaParameterInfo parameterInfo, JavaSettings settings) {
+				// TODO
+			}
+
+			@Override
+			public LambdaParameterInfo evaluate(LambdaParameterInfo parameterInfo, String parameterName, JavaSettings settings) throws SemanticException, EvaluationException {
+				parameterInfo.addParameter(parameterName);
+				return parameterInfo;
+			}
+		};
+	}
 }
